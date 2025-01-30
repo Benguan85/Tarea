84c8ccd86ca65d17c58dea3a3a15b33c3451e546
Benguan85 <benguan18@hotmail.com>
2025-01-30 09:12:45 +0100

    Prueba dia 30 enero
    Signed-off-by: Benguan85 <benguan18@hotmail.com>

---
+++ T3.49/src/for49.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class for49 {
+
+	public static void main(String[] args) {
+		int psoF, psoR, paga = 0, totalmaleta, total = 0, coste;
+		Scanner s = new Scanner(System.in);
+		System.out.print("Cuántas maletas llevas: ");
+		totalmaleta = s.nextInt();
+		for (int numMaleta = 1; numMaleta <= totalmaleta; numMaleta++) {
+			coste = 0;
+			System.out.print("Introduce peso máximo facturado: ");
+			psoF = s.nextInt();
+			System.out.print("Introduce peso de la maleta: ");
+			psoR = s.nextInt();
+			paga = psoR - psoF;
+
+			if (paga > 0) {
+				coste = 12 * paga;
+			}
+			total += coste;
+		}
+		System.out.println("Coste total: " + total + " €");
+	}
+
+}
